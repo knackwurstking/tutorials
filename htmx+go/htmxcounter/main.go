a81eba8262af895821298e8130e99d1dacf2c9cc
Udo Bauer <knackwurstking.tux@gmail.com>
2024-02-06 04:22:52 +0100

    htmxcounter: remove README, add assets dir and update index and main

--- htmx+go/htmxcounter/main.go
+++ htmx+go/htmxcounter/main.go
@@ -1,14 +1,20 @@
 package main
 
 import (
+	"html/template"
+
 	"github.com/gin-gonic/gin"
 )
 
 func main() {
 	r := gin.Default()
 
+	r.Static("/assets", "./assets")
+
 	r.GET("/", func(c *gin.Context) {
-		c.Writer.Write([]byte("Hello World!"))
+		//c.Writer.Write([]byte("Hello World!"))
+		templ, _ := template.ParseFiles("index.html")
+		templ.ExecuteTemplate(c.Writer, "index.html", nil)
 	})
 
 	r.Run() // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
