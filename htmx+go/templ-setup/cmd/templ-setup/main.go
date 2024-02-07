f50e8274292aa957c49ef368517b2a789fce825f
Udo Bauer <knackwurstking.tux@gmail.com>
2024-02-07 17:30:01 +0100

    templ-setup: adding logger middleware

--- htmx+go/templ-setup/cmd/templ-setup/main.go
+++ htmx+go/templ-setup/cmd/templ-setup/main.go
@@ -4,6 +4,7 @@ import (
 	"templ-setup/handler"
 
 	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
 )
 
 type DB struct {
@@ -11,6 +12,14 @@ type DB struct {
 
 func main() {
 	app := echo.New()
+
+	defaultLoggerConfig := middleware.DefaultLoggerConfig
+	defaultLoggerConfig.Format = "[${time_rfc3339}] ${method} ${uri} ${status}\n"
+	defaultLoggerConfig.Output = app.Logger.Output()
+
+	app.Use(
+		middleware.LoggerWithConfig(defaultLoggerConfig),
+	)
 
 	userHandler := handler.UserHandler{}
 	app.GET("/user", userHandler.HandleUserShow)
