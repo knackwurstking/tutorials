ac9755c116cf9d381f1ac87cd15828101c5db5a2
Udo Bauer <knackwurstking.tux@gmail.com>
2024-02-07 18:03:00 +0100

    templ-setup: complete...

--- htmx+go/templ-setup/cmd/templ-setup/main.go
+++ htmx+go/templ-setup/cmd/templ-setup/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"context"
 	"templ-setup/handler"
 
 	"github.com/labstack/echo/v4"
@@ -19,10 +20,19 @@ func main() {
 
 	app.Use(
 		middleware.LoggerWithConfig(defaultLoggerConfig),
+		withUser,
 	)
 
 	userHandler := handler.UserHandler{}
 	app.GET("/user", userHandler.HandleUserShow)
 
 	app.Start(":3000") // NOTE: dev port number 3000
+}
+
+func withUser(next echo.HandlerFunc) echo.HandlerFunc {
+	return func(c echo.Context) error {
+		ctx := context.WithValue(c.Request().Context(), "user", "knackwurstking@mail.com")
+		c.SetRequest(c.Request().WithContext(ctx))
+		return next(c)
+	}
 }
