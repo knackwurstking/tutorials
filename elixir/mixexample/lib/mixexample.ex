3255876503900a9e49338d764c01a0b5226bbcb2
Udo Bauer <knackwurstking.tux@gmail.com>
2024-04-23 18:21:38 +0200

    Addd UUID elixir module

--- elixir/mixexample/lib/mixexample.ex
+++ elixir/mixexample/lib/mixexample.ex
@@ -23,9 +23,10 @@
 
 defmodule Mixexample do
   use Application
+  alias UUID
 
   def start(_type, _args) do
-    IO.puts(hello())
+    IO.puts(UUID.uuid4())
     Supervisor.start_link([], strategy: :one_for_one)
   end
 
