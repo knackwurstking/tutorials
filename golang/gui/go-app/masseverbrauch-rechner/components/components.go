aacb213a962173a444590eb4b0d7084334e53d3c
Udo Bauer <knackwurstking.tux@gmail.com>
2025-02-21 20:31:45 +0100

    remove calculator component

--- golang/gui/go-app/masseverbrauch-rechner/components/components.go
+++ golang/gui/go-app/masseverbrauch-rechner/components/components.go
@@ -1,5 +1,1 @@
 package components
-
-import "masseverbrauch-rechner/components/calculator"
-
-type Calculator = calculator.Calculator
--- golang/gui/go-app/masseverbrauch-rechner/pages/root.go
+++ golang/gui/go-app/masseverbrauch-rechner/pages/root.go
@@ -4,8 +4,6 @@ import (
 	"log/slog"
 
 	"github.com/maxence-charriere/go-app/v10/pkg/app"
-
-	"masseverbrauch-rechner/components"
 )
 
 type Root struct {
@@ -15,15 +13,11 @@ type Root struct {
 func (r *Root) Render() app.UI {
 	slog.Debug("Rendering page", "name", "Root")
 
-	calc := &components.Calculator{} // TODO: Forget about this calc component
-
-	// TODO: Create form inputs needed for calculation
-
 	return app.Div().
 		Class("ui-debug").
 		Styles(map[string]string{
 			"width":  "100%",
 			"height": "100%",
 		}).
-		Body(calc.Render())
+		Body() // TODO: Create form inputs needed for calculation
 }
